6d009e63788ec6235689ecdfef222e3091894cba
Andrew Crow <andrew@crow.me>
2020-10-23 18:54:44 -0700

    added redirect for create post page

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -10,13 +10,14 @@ router.get("/userpost", function (req, res) {
 router.post("/userpost", function (req, res) {
     console.log(req.body);
     db.Post.create({
+        user_name: req.body.user_name,
         title: req.body.title,
         description: req.body.description,
         beer_name: req.body.beer_name,
         brewery_name: req.body.brewery_name,
         image: req.body.image
     }).then(newPost => {
-        res.json(newPost)
+        res.redirect("/");
       }).catch(err => {
         console.log(err)
         res.status(500).json(err);
