8576568099ca211a02d5b8d191b97456d8a04743
Andrew Crow <andrew@crow.me>
2020-10-23 20:03:10 -0700

    restrciting access

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -14,7 +14,7 @@ router.post('/signup', (req, res) => {
         password: req.body.password
     }).then(newUser => {
         const newUserJson = newUser.toJSON();
-        const hbsObject = {user : newUserJson};
+        const hbsObject = {userNew : newUserJson};
         return res.render("userlogin", hbsObject);
     }).catch(err => {
         console.log(err);
